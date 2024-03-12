286f9a4816423322c0e308184808f73225c17490
Ubuntu <ubuntu@ip-172-31-15-8.ap-northeast-2.compute.internal>
2024-03-12 01:51:46 +0000

    build 해봄. 그리고 process wait 처리

--- userprog/process.c
+++ userprog/process.c
@@ -204,6 +204,12 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+	while (1)
+	{
+		/* code */
+	}
+	
+
 	return -1;
 }
 
